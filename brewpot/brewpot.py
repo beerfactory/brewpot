a7492cae1ad4ab40f43d9f49c0565514de0b0cfe
Nico <nicolas.jouanin@gmail.com>
2013-12-09 23:04:04 +0100

    Add dummy callback for tests

--- brewpot/brewpot.py
+++ brewpot/brewpot.py
@@ -1,5 +1,8 @@
 from core.framework import base as framework
+from core.framework.events import FrameworkStartedEvent
+from core.events import AnyEvent
 import logging
+import time
 
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s',
@@ -7,7 +10,14 @@ logging.basicConfig(level=logging.DEBUG,
 )
 logger = logging.getLogger('brewpot')
 
+
+def callback(event):
+    logger.info("event received %s", str(type(event)))
+    time.sleep(1)
+
+
 if __name__ == '__main__':
     logger.info("starting")
     fwk = framework.newFramework(dict())
+#    fwk.register_event_callback(fwk.get_context(), callback, [FrameworkStartedEvent, AnyEvent])
     fwk.start()
