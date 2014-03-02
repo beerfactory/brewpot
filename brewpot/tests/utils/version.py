4b60449fbd76c3ceb42071866a9fa8336b98da17
Nico <nicolas.jouanin@gmail.com>
2014-03-02 20:30:47 +0100

    Rename test methods

--- brewpot/tests/utils/version.py
+++ brewpot/tests/utils/version.py
@@ -4,12 +4,12 @@ from brewpot.utils import version
 
 class VersionTestFunction(unittest.TestCase):
 
-    def test_versionfinal(self):
+    def test_version_final(self):
         v = (0, 1, 0, 'final', 0)
         v_str = version.get_version(v)
         self.assertEqual('0.1', v_str)
 
-    def test_versionalpha(self):
+    def test_version_alpha(self):
         v = (0, 1, 0, 'alpha', 1)
         v_str = version.get_version(v)
         self.assertEqual('0.1a1', v_str)
